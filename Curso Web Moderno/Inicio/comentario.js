c4154bed78ad0ff0c222bed5ed4383ad68e117b1
Hugo Ferreira Da Silva <81571386+hugoleao02@users.noreply.github.com>
2023-05-12 14:52:41 -0300

    Add files via upload

---
+++ Curso Web Moderno/Inicio/comentario.js
@@ -0,0 +1,13 @@
+//console. log é usado para exibir algo na tela  do computador
+console.log("Passo # 02");
+console.log("Passo # 01");// mais um exemplo de console.log
+//console.log("Passo # 03");
+
+/**
+ * Esse é um 
+ * comentário
+ * de multipla
+ * linha!
+ */
+
+console.log("Passo # 03");
---
+++ Curso Web Moderno/Inicio/sentenca_1.js
@@ -0,0 +1,4 @@
+console
+    .log("Bom dia!")
+console.log("Bao tarde!")
+console.log("Boa noite!")
