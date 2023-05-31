d5adfd59740d6047864f52f2d479c9a7e722ec1b
Hugo Ferreira Da Silva <81571386+hugoleao02@users.noreply.github.com>
2023-05-31 08:15:23 -0300

    React

---
+++ react/exercicios/src/componentes/BomDia.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+
+export default props =>
+    <React.Fragment>
+        <h1 key='h1'>Bom dia {props.nome}!</h1>
+        <h2 key='h2'>Até breve!</h2>
+    </React.Fragment>
